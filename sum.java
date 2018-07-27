5e6209b81fbdc2399a12813e4e75844503caae67
puvvadamaniharish <41737685+puvvadamaniharish@users.noreply.github.com>
2018-07-27 23:56:13 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   Scanner g = new Scanner (System.in);
+		int f = g.nextInt();
+		int k = ((f*(f+1))/2);
+		System.out.println(k);  
+		 
+	}
+}
